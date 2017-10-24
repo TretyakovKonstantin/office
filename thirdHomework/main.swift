0257353269a897b8db3310423df454cc77689bb7
Tester Apps <apps@studiomobile.ru>
2017-10-24 10:58:45 +0300

    Coockies problem solving added

--- thirdHomework/main.swift
+++ thirdHomework/main.swift
@@ -1,11 +1,52 @@
 import Foundation
 
 class OfficeManager {
+    func checkOffice(office: Office) throws {
+        try office.getCookiesCount()
+    }
     
+    func addCookies(office: Office, count: Int) {
+        var newCookies:[Cookie] = []
+        for _ in 0...count {
+            newCookies += [Cookie()]
+        }
+        office.addCookies(cookies:newCookies)
+    }
 }
 
+
 class Office {
+    private var officeManager: OfficeManager?
+    private var cookies:[Cookie]?
     
+    
+    func getCookiesCount() throws -> (Int) {
+        guard cookies != nil && cookies!.count > 0 else {
+            throw officeProblems.cookiesAbsence
+        }
+        return cookies!.count
+    }
+    
+    func addCookies(cookies: [Cookie]) {
+        if self.cookies == nil {
+            self.cookies = []
+        }
+        self.cookies! += cookies
+    }
+    
+    init(manager: OfficeManager) {
+        self.officeManager = manager
+    }
+    
+    func checkOfficeProblems() {
+        do {
+            try officeManager?.checkOffice(office: self)
+        } catch officeProblems.cookiesAbsence {
+            officeManager?.addCookies(office: self, count: 15)
+        } catch {
+            
+        }
+    }
 }
 
 enum officeProblems: Error {
@@ -15,4 +56,8 @@ enum officeProblems: Error {
     case cookiesAbsence
     case newMailInAForeinLanguage
     case actOfTerrorism
+}
+
+class Cookie {
+    
 }
